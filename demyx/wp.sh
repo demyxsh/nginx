c0cb72d546f796dd53de62dc9c7b5c5c7fe2c439
Demyx <info@demyx.sh>
2019-12-05 02:20:32 -0800

    Remove security header causing errors in browser console

--- demyx/wp.sh
+++ demyx/wp.sh
@@ -114,7 +114,6 @@ http {
   }
 
   add_header X-Powered-By \"Demyx\";
-  add_header Content-Security-Policy \"default-src 'self';\";
   add_header X-Frame-Options \"SAMEORIGIN\";
   add_header X-XSS-Protection  \"1; mode=block\" always;
   add_header X-Content-Type-Options \"nosniff\" always;
